dd60e9f1193be3eb619e59dc90f2d3b30f218cd3
kortfolio <kangm210@gmail.com>
2019-05-26 18:57:21 -0700

    [Update] Minor UI Updates
    • Updated Favicon
    • Removed unnecessary markups.

--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { ThemeContextProvider } from "./src/theme-context"
-
-export const wrapRootElement = ({ element }) => (
-  <ThemeContextProvider>{element}</ThemeContextProvider>
-);
